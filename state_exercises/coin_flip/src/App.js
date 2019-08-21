d01937d4c4b46b65fc4ef4f01fb9054a3d10d580
jlyell <jlyell@kpmg.com>
2019-08-21 00:31:47 -0400

    fix bug and coin flip start

---
+++ state_exercises/coin_flip/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Flip from './Flip';
+
+function App() {
+  return (
+    <div className="App">
+      <Flip />
+    </div>
+  );
+}
+
+export default App;
---
+++ state_exercises/coin_flip/src/Flip.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Coin from './Coin';
+
+class Flip extends Component {
+    render(){
+        return(
+            <div>
+                <Coin />
+            </div>
+        )
+    }
+}
+
+export default Flip;
