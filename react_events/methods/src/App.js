6ccb66a08aa2f12f4e6c0696013e82775dd77a2f
lylax47 <jjlyell@gmail.com>
2019-12-18 23:44:25 -0500

    react events

---
+++ react_events/methods/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NumberList from './NumberList';
+
+function App() {
+  return (
+    <div className="App">
+      < NumberList/>
+    </div>
+  );
+}
+
+export default App;
